0c56cf253e307e8052e29316ae89a395624f8a0c
Ayaminn <ayamin.imai@gmail.com>
2016-02-10 01:04:36 +0900

    1日1回やったって画面に戻るボタンが実装されたよ

--- OMIKUJI/OneDayController.swift
+++ OMIKUJI/OneDayController.swift
@@ -9,20 +9,31 @@
 import UIKit
 
 class OneDayController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //        let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("mikuji") as! UIViewController
-        //        self.presentViewController( targetViewController, animated: true, completion: nil)
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func returnButton(sender: AnyObject) {
-        self.performSegueWithIdentifier("returnMenu", sender: nil)
+    
+    @IBAction func returnMenu(sender: UIStoryboardSegue) {
+       performSegueWithIdentifier("screenTransition", sender: self)
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
